bade6a87067017a18432a71d2519d813fb6a08c1
Kiran Kumar <kirankumar.mekala2003@gmail.com>
2024-06-17 22:53:08 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = "World";
+  return (
+    <div>
+      Hello {value} 👋
+      <h2>I'm Kiran, This is my first wrokshop of react 👨‍💻</h2>
+      <p>It's was amazing 😎</p>
+    </div>
+  );
 }
 
 export default App;
